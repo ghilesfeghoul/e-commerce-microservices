a91696ab8375d014b6140f9ed1199c38be7f1541
gfeghoul <ghiles.feghoul@twenga.com>
2023-07-27 14:31:03 +0200

    use rabbitmq instead of kafka as a driver queue

--- email-ms/app/Providers/EventServiceProvider.php
+++ email-ms/app/Providers/EventServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Jobs\OrderCompleted;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -25,7 +26,7 @@ class EventServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        \App::bindMethod(OrderCompleted::class . '@handle', fn($job) => $job->handle());
     }
 
     /**
